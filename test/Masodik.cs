63353063a62d993c9d5a6192cb8878eb16fe3a5c
Bálint <gyarfas.balint@petrik.hu>
2026-01-29 13:33:36 +0100

    masodik osztaly kesz

---
+++ test/Masodik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+	internal class Masodik
+	{
+		List<Adat> list;
+
+		public Masodik(List<Adat> list)
+		{
+			list = new List<Adat>();
+		}
+	}
+}
