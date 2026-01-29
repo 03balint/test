80519875b8b699c232da98e1269c2d9f0cdd3b4c
Petrik Diak <diak@petrik.hu>
2026-01-29 13:29:41 +0100

    adat osztaly kesz

---
+++ test/Adat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+	internal class Adat
+	{
+		private string nev;
+		private int ar;
+
+		public Adat(string nev)
+		{
+			this.nev = nev;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Ar { get => ar; set => ar = value; }
+	}
+}
